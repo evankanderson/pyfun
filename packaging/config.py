2896ed9141ba87e722d45b3f868221f4ef001e45
Evan Anderson <argent@google.com>
2019-01-17 09:30:28 +0000

    Get knative build working in packaging/

--- packaging/config.py
+++ packaging/config.py
@@ -1,6 +1,8 @@
 from glob import glob
 import importlib
 import os.path
+# This is needed to start gunicorn
+from pyfun_events.run import app
 
 for f in glob('*.py'):
     if not os.path.isfile(f):
