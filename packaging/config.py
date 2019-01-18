92ec771342d6ddbcf38b5921dc672cd371a1236a
Evan Anderson <argent@google.com>
2019-01-18 18:55:27 +0000

    Fix packaging to send application logs to STDOUT from dockerfile by default.

--- packaging/config.py
+++ packaging/config.py
@@ -6,9 +6,13 @@ import os.path
 from pyfun_events.run import app
 
 if __name__ != '__main__':
+    # Ensure logs go to the configured gunicorn location.
     gunicorn_logger = logging.getLogger('gunicorn.error')
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
+    root_logger = logging.getLogger()
+    root_logger.handlers = gunicorn_logger.handlers
+    root_logger.setLevel(gunicorn_logger.level)
 
 for f in glob('*.py'):
     if not os.path.isfile(f):
